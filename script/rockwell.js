a88e160f91f48155cf9c0a1bc662c12f27eec63d
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-11-06 15:11:49 -0800

    Create rockwell.js

---
+++ script/rockwell.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  
+});
+
+var players = [
+  {
+    name: 'Advik',
+    points: 0
+  }, {
+    name: 'Aren',
+    points: 0
+  }, {
+    name: 'Charles',
+    points: 0
+  }, {
+    name: 'Connor',
+    points: 0
+  }, {
+    name: 'Dorian',
+    points: 0
+  }, {
+    name: 'Eugene',
+    points: 0
+  }, {
+    name: 'Evan',
+    points: 0
+  }, {
+    name: 'Smitty',
+    points: 0
+  }, {
+    name: 'Josh T.',
+    points: 0
+  }, {
+    name: 'Luke',
+    points: 0
+  }, {
+    name: 'Martin',
+    points: 0
+  }, {
+    name: 'Nick',
+    points: 0
+  }, {
+    name: 'Noah',
+    points: 0
+  }, {
+    name: 'Ryder',
+    points: 0
+  }, {
+    name: 'Traeger',
+    points: 0
+  }
+];
+
+function makeInputTable (action) {
+  var table = $('#inputTable');
+  if (action === undefined) {
+    
+  }
+}
