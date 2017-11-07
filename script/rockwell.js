1ac5bab5a96182a16fb90be6d0e5345963d9c916
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-11-06 19:21:31 -0800

    Update rockwell.js

--- script/rockwell.js
+++ script/rockwell.js
@@ -1,59 +1,26 @@
 $(document).ready(function () {
-  
+  makeInputTable();
 });
 
-var players = [
-  {
-    name: 'Advik',
-    points: 0
-  }, {
-    name: 'Aren',
-    points: 0
-  }, {
-    name: 'Charles',
-    points: 0
-  }, {
-    name: 'Connor',
-    points: 0
-  }, {
-    name: 'Dorian',
-    points: 0
-  }, {
-    name: 'Eugene',
-    points: 0
-  }, {
-    name: 'Evan',
-    points: 0
-  }, {
-    name: 'Smitty',
-    points: 0
-  }, {
-    name: 'Josh T.',
-    points: 0
-  }, {
-    name: 'Luke',
-    points: 0
-  }, {
-    name: 'Martin',
-    points: 0
-  }, {
-    name: 'Nick',
-    points: 0
-  }, {
-    name: 'Noah',
-    points: 0
-  }, {
-    name: 'Ryder',
-    points: 0
-  }, {
-    name: 'Traeger',
-    points: 0
-  }
-];
+var players = [{"name":"Advik","points":0},{"name":"Aren","points":0},{"name":"Charles","points":0},{"name":"Connor","points":0},{"name":"Dorian","points":0},{"name":"Eugene","points":0},{"name":"Evan","points":0},{"name":"Smitty","points":0},{"name":"Josh T.","points":0},{"name":"Luke","points":0},{"name":"Martin","points":0},{"name":"Nick","points":0},{"name":"Noah","points":0},{"name":"Ryder","points":0},{"name":"Traeger","points":0}];
 
 function makeInputTable (action) {
   var table = $('#inputTable');
+  var i;
+  var string = '';
   if (action === undefined) {
-    
+    for (i = 0; i < players.length; i++) {
+      if (i % 3 === 0) {
+        string += '<tr><td>' + players[i].name + '</td>';
+      } else if (i % 3 !== 0) {
+        string += '<td>' + players[i].name + '</td>';
+      }
+      //alert(players[i].name);
+      //else {
+        //table.append('<td>' + players[i].name + '</td>');
+      //}
+    }
+    string += '</tr>';
+    table.html(string);
   }
 }
