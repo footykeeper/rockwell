a3118752d6981a27d9db9251aeb4e40ca82767bb
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-11-26 16:13:37 -0800

    Update card-editor.js

--- script/card-editor.js
+++ script/card-editor.js
@@ -4,11 +4,16 @@ function fifaBadge (element) {
   ctx.drawImage(badge, 0, 0);
   this.points = function (number) {
     ctx.font = "25px Arial";
-    ctx.fillText(number, 3 ,55);
+    ctx.fillText(number, 3, 70);
   };
   
   this.name = function (name) {
     ctx.font = "15px Arial";
     ctx.fillText(name, 35, 135);
+  };
+  
+  this.position = function (position) {
+    ctx.font = "15px Arial";
+    ctx.fillText(position, 2, 90);
   };
 }
