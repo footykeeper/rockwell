4f86f577f6d63ab3acf791a9bee7d13afb00ebe1
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-17 15:31:05 -0800

    Create builder.js

---
+++ rosters/builder.js
@@ -0,0 +1,31 @@
+function addPlayer () {
+  var lineBreak = '<br/><br/>';
+  var playerInput = '<input type="text" placeholder="Player Name" class="name"/>';
+  $('#inputs').append(lineBreak + playerInput);
+}
+
+function createRoster () {
+  var inputs = document.getElementsByClassName('name');
+  var i;
+  var roster = '[{';
+  
+  for (i = 0; i < inputs.length; i++) {
+    if (i === 0) {
+      roster += '"name":"' + inputs[i].value + '","points":0}';
+    } else {
+      roster += ',{"name":"' + inputs[i].value + '","points":0}';
+    }
+  }
+  
+  roster += ']';
+  return roster;
+}
+
+$('#playerAdd').click(function () {
+  addPlayer();
+});
+
+$('#create').click(function () {
+  $('#final').show();
+  $('#display').text(createRoster());
+});
