98fe1a5cc3a24597cd129c1e9ba25eb30c600948
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-24 14:14:36 -0800

    Update builder.php

--- rosters/builder.php
+++ rosters/builder.php
@@ -7,13 +7,16 @@ function addPlayer(){$("#inputs").append('<br/><br/><input type="text" placehold
         $data = htmlspecialchars($data);
         return $data;
     }
-    
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
             // Do nothing
         } else {
             // The user is signed in; do cool stuff
-            $sql = 'INSERT INTO rosters (creator_name, roster_string) VALUES (' . $username . ',' . secure_input($_POST['data']) . ')';
+            $sql = 'INSERT INTO rosters (creator_name, roster_string) VALUES ("' . $username . '", "' . secure_input($_POST['data']) . '")';
+            if ($link->query($sql) === TRUE) {
+                echo "<div class='w3-green w3-container'><p>Roster Saved!</p></div>";
+            }
         }
     }
 ?>
