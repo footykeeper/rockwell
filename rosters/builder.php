9f741bf67c2c77b8f2d050f1d082fc57be80f04b
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-29 15:48:20 -0800

    Update builder.php

--- rosters/builder.php
+++ rosters/builder.php
@@ -1,5 +1,4 @@
 <?php
-
     echo '<script>' . file_get_contents('builder.min.js') . '</script>';
 
     function secure_input ($data) {
@@ -10,11 +9,12 @@
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $string = secure_input($_POST['data']);
         if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
             // Do nothing
         } else {
             // The user is signed in; do cool stuff
-            $sql = 'INSERT INTO rosters (creator_name, roster_string) VALUES ("' . $username . '", "' . secure_input($_POST['data']) . '")';
+            $sql = 'INSERT INTO rosters (creator_name, roster_string) VALUES ("' . $username . '", "' . $string . '")';
             if ($link->query($sql) === TRUE) {
                 echo "<br/><br/><div class='w3-green w3-container'><p>(alpha phase) Roster saved to your account, " . $username . "!</p></div>";
             }
