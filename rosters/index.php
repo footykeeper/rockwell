f5629678e4035f5333eecc933d159f12402e3974
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-30 12:38:05 -0800

    Update index.php

--- rosters/index.php
+++ rosters/index.php
@@ -12,26 +12,31 @@ require_once 'builder.php';
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
   <title>Roster Creator // Footykeeper</title>
 </head>
-<body class="w3-container">
-  <br/>
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-    <div id="inputs">
-      <input type="text" placeholder="Player Name" class="name"/><br/><br/>
-      <input type="text" placeholder="Player Name" class="name"/><br/><br/>
-      <input type="text" placeholder="Player Name" class="name"/><br/><br/>
-      <input type="text" placeholder="Player Name" class="name"/><br/><br/>
-      <input type="text" placeholder="Player Name" class="name"/>
-      <input id="string" name="data" hidden/>
+<body>
+  <?php
+  require_once '../navigation.php';
+  ?>
+  <div class="w3-container">
+    <br/>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+      <div id="inputs">
+        <input type="text" placeholder="Player Name" class="name"/><br/><br/>
+        <input type="text" placeholder="Player Name" class="name"/><br/><br/>
+        <input type="text" placeholder="Player Name" class="name"/><br/><br/>
+        <input type="text" placeholder="Player Name" class="name"/><br/><br/>
+        <input type="text" placeholder="Player Name" class="name"/>
+        <input id="string" name="data" hidden/>
+      </div>
+      <hr/>
+      <input type="submit" id="sendRoster" hidden/>
+    </form>
+    <button class="w3-button w3-blue" id="create">Create Roster</button>
+    <br/><br/>
+    <button class="w3-button w3-blue" id="playerAdd">Add Player</button>
+    <div id="final" hidden>
+      <p>Your Roster:</p>
+      <p id="display"></p>
     </div>
-    <hr/>
-    <input type="submit" id="sendRoster" hidden/>
-  </form>
-  <button class="w3-button w3-blue" id="create">Create Roster</button>
-  <br/><br/>
-  <button class="w3-button w3-blue" id="playerAdd">Add Player</button>
-  <div id="final" hidden>
-    <p>Your Roster:</p>
-    <p id="display"></p>
   </div>
   <script src="builder.min.js"></script>
 </body>
