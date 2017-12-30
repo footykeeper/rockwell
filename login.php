b6714321dbd17b44e1d6c107fecb6861c3965dc9
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-30 12:50:54 -0800

    Update login.php

--- login.php
+++ login.php
@@ -80,6 +80,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
