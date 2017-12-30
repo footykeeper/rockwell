2e6ec72515b6a4b7cde2fff5a26859bb38297de8
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2017-12-30 12:47:09 -0800

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,23 +1,25 @@
-class url {
-  public function __construct ($name, $href) {
-    $this->name = $name;
-    $this->href = $href;
+<?php
+  class url {
+    public function __construct ($name, $href) {
+      $this->name = $name;
+      $this->href = $href;
+    }
   }
-}
 
-if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
-  // User is not signed in
-  $urls = array(new url('Home', '/'), new url('Tracker', '/tracker/'), new url('Roster Creator', '/rosters/'), new url('Log In', '/login.php'), new url('Register', '/register.php'));
-} else {
-  // User is signed in
-  $urls = array(new url('Home', '/'), new url('Tracker', '/tracker/'), new url('Roster Creator', '/rosters/'), new url('Log Out', '/logout.php'));
-}
+  if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+    // User is not signed in
+    $urls = array(new url('Home', '/'), new url('Tracker', '/tracker/'), new url('Roster Creator', '/rosters/'), new url('Log In', '/login.php'), new url('Register', '/register.php'));
+  } else {
+    // User is signed in
+    $urls = array(new url('Home', '/'), new url('Tracker', '/tracker/'), new url('Roster Creator', '/rosters/'), new url('Log Out', '/logout.php'));
+  }
 
 
-echo '<div class="w3-bar">';
+  echo '<div class="w3-bar">';
 
-foreach ($urls as $value) {
-  echo '<a href="' . $value->href . '"><div class="w3-button">' . $value->name . '</div></a>';
-}
+  foreach ($urls as $value) {
+    echo '<a href="' . $value->href . '"><div class="w3-button">' . $value->name . '</div></a>';
+  }
 
-echo '</div>';
+  echo '</div>';
+?>
