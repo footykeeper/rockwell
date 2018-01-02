37471e187dda2c87719f995d82b682dde8b23cf4
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-01-02 11:26:40 -0800

    Update index.php

--- index.php
+++ index.php
@@ -35,39 +35,6 @@ $username = $_SESSION['username'];
     </div>
     <p class="w3-xxlarge"><span class="switch" id="left">&laquo;</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="switch" id="right">&raquo;</span></p>
   </div>
-  <script>
-var step = {
-  index: 0
-};
-$(document).ready(function () {
-  var steps = document.getElementsByClassName('stepDetails');
-  var i;
-  for (i = 0; i < steps.length; i++) {
-    $(steps[i]).hide();
-  }
-  $(steps[step.index]).show();
-});
-$('.switch').click(function () {
-  var direction = $(this).attr('id');
-  var steps = document.getElementsByClassName('stepDetails');
-  var i;
-  
-  if (direction === 'right') {
-    if (step.index < 2) {
-      step.index++;
-    }
-  } else if (direction === 'left') {
-    if (step.index > 0) {
-      step.index--;
-    }
-  }
-  
-  for (i = 0; i < steps.length; i++) {
-    $(steps[i]).hide();
-  }
-  
-  $(steps[step.index]).show();
-});
-  </script>
+  <script src="homepage-scripts/slideshow.min.js"></script>
 </body>
 </html>
