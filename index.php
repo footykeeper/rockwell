c292c4f6a0eec03f180aa839ac5c0b82e571f693
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-01-02 11:04:13 -0800

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $username = $_SESSION['username'];
   </div>
   <div class="w3-container w3-center w3-blue">
     <p class="w3-xxlarge">Recording Stats Is Easy as <span class="step" id="firstStep">1</span> - <span class="step" id="secondStep">2</span> - <span class="step" id="thirdStep">3</span>, and It's All Free!</p>
-    <div class="stepDetails" hidden>
+    <div class="stepDetails">
       <p class="w3-xlarge">Step 1: Sign Up</p>
       <p class="w3-large">To begin, go to the <a href="register.php">Register</a> page to create your free account with unlimited rosters, games, and access to all Footykeeper features.</p>
     </div>
@@ -26,9 +26,9 @@ $username = $_SESSION['username'];
       <p class="w3-xlarge">Step 2: Create Your Roster</p>
       <p class="w3-large">After creating your free account, head over to the <a href="rosters/">Roster Creator</a> to create and save your roster to your account! The roster creator interface is simple and easy to understand.</p>
     </div>
-    <div class="stepDetails">
+    <div class="stepDetails" hidden>
       <p class="w3-xlarge">Step 3: Start recording stats</p>
-      <p class="w3-large">Now that your free account has been created and you have a roster saved, go to the <a href="tracker/">Tracker</a> </p>
+      <p class="w3-large">Now that your free account has been created and you have a roster saved, go to the <a href="tracker/">Tracker</a>, select one of your teams, and begin recording events such as (hopefully) goals, assists, saves, and (hopefully not) bookings.</p>
     </div>
     <p class="w3-large"><span class="switch" id="left">&laquo;</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="switch" id="right">&raquo;</span></p>
   </div>
