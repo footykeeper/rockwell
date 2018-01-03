6ee0e0439bc2d4b0e86f8bb35c615cd303bf0307
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-01-02 18:20:22 -0800

    Update index.php

--- rosters/index.php
+++ rosters/index.php
@@ -7,6 +7,7 @@ require_once 'builder.php';
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="../media/images/favicon.ico"/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
