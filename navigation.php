3989bfedfff10925feea3433fe645355947d76e9
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2018-01-02 18:26:53 -0800

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -8,7 +8,7 @@
 
   if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
     // User is not signed in
-    $urls = array(new url('Home', '/'), new url('Tracker', '/tracker/'), new url('Roster Creator', '/rosters/'), new url('Log In', '/login.php'), new url('Register', '/register.php'));
+    $urls = array(new url('Home', '/'), new url('Log In', '/login.php'), new url('Register', '/register.php'));
   } else {
     // User is signed in
     $urls = array(new url('Home', '/'), new url('Tracker', '/tracker/'), new url('Roster Creator', '/rosters/'), new url('Log Out', '/logout.php'));
